1df930b3de71853beda1459e60cb873280ba4140
exsemt <exsemt@gmail.com>
2014-05-09 14:42:45 +0200

    i18n

--- config/initializers/spree_i18n.rb
+++ config/initializers/spree_i18n.rb
@@ -1,2 +1,2 @@
-SpreeI18n::Config.available_locales = [:de, :en] # displayed on translation forms
+SpreeI18n::Config.available_locales = [:de] # displayed on translation forms
 SpreeI18n::Config.supported_locales = [:de] # displayed on frontend select box
